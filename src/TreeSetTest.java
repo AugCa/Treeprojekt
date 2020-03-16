aa2a3dc5b2bc8dbf3d5679840882eb97f974e71a
Adan Anwar <adananwar71@gmail.com>
2020-03-16 16:42:15 +0100

    Added new test classes for the TreeSet

--- src/TreeSetTest.java
+++ src/TreeSetTest.java
@@ -1,24 +1,82 @@
-import org.testng.annotations.Test;
-
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeSet;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TreeSetTest {
+
     @Test
-    public static class ReversedIntegerComparator implements Comparator {
-        public int compare(Object o1, Object o2) {
-            return -(((Integer) o1).compareTo((Integer) o2));
-        }
-        public boolean equals(Object o1, Object o2) {
-            return ((Integer) o1).compareTo((Integer) o2) == 0;
-        }
+    void comparator() {
     }
 
+    @Test
+    void subSet() {
+    }
+
+    @Test
+    void headSet() {
+    }
+
+    @Test
+    void tailSet() {
+    }
+
+    @Test
+    void first() {
+    }
+
+    @Test
+    void last() {
+    }
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void isEmpty() {
+    }
+
+    @Test
+    void contains() {
+    }
+
+    @Test
+    void iterator() {
+    }
+
+    @Test
+    void toArray() {
+    }
+
+    @Test
+    void testToArray() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void containsAll() {
+    }
+
+    @Test
+    void addAll() {
+    }
+
+    @Test
+    void retainAll() {
+    }
+
+    @Test
+    void removeAll() {
+    }
+
+    @Test
+    void clear() {
+    }
 }
